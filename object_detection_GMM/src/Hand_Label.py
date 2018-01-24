dd883734a7106a36ec7bb46c88d0f2e36ed30dd8
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-01-24 10:29:29 -0500

    refine the main code

--- object_detection_GMM/src/Hand_Label.py
+++ object_detection_GMM/src/Hand_Label.py
@@ -4,24 +4,29 @@ import pylab as pl
 from roipoly import roipoly
 from matplotlib import pyplot as plt
 
+
 # store pixel within in barrel region
 def getdata(mask, imgx, row, col):
-    for i in range (row):
-        for j in range (col):
-            if mask[i, j]:
-                res.append([imgx[i, j, 0], imgx[i, j, 1], imgx[i, j, 2]])
+  for i in range (row):
+    for j in range (col):
+      if mask[i, j]:
+        res.append([imgx[i, j, 0], imgx[i, j, 1], imgx[i, j, 2]])
 
-# main code
-folder = "otherRed2"  # the folder stores training images
-res = []
-for filename in os.listdir(folder):
+
+def main():
+  # main code
+  folder = "otherRed2"  # the folder stores training images
+  res = []
+  for filename in os.listdir(folder):
+    # the default read-in channel order is BGR
     img = cv2.imread(os.path.join(folder, filename))
     imgx = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # use HSV color base
     
     row = imgx.shape[0]
     col = imgx.shape[1]
-    # show the image
-    pl.imshow(imgx, interpolation = 'nearest', cmap = "hsv")
+
+    # plot out the image
+    pl.imshow(imgx, interpolation='nearest', cmap="hsv")
     pl.colorbar()
     pl.title("left click: line segment         right click: close region")
 
@@ -29,8 +34,10 @@ for filename in os.listdir(folder):
     ROIim = roipoly(roicolor = 'r')
     getdata(ROIim.getMask(imgx[:, :, 0]), imgx, row, col)
 
-# store training data
-np.save('data_redOtherK2.npy', res)
 
-    
+  # store training data
+  np.save('data_redOtherK2.npy', res)
 
+
+if __name__ == "__main__":
+  main()
