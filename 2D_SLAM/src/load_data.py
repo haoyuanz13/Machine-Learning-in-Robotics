042f704dabb2390aece9b174f6795a9bea3afca5
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-02-21 12:14:34 -0500

    Update load_data.py

--- 2D_SLAM/src/load_data.py
+++ 2D_SLAM/src/load_data.py
@@ -18,51 +18,46 @@ from scipy import io
 
 
 def get_lidar(file_name):
-	data = io.loadmat(file_name+".mat")
-	lidar = []
-	for m in data['lidar'][0]:
-		tmp = {}
-		tmp['t']= m[0][0][0]
-		nn = len(m[0][0])
-		if (nn != 5) and (nn != 6):			
-			raise ValueError("different length!")
-		tmp['pose'] = m[0][0][nn-4]
-		tmp['res'] = m[0][0][nn-3]
-		tmp['rpy'] = m[0][0][nn-2]
-		tmp['scan'] = m[0][0][nn-1]
-		
-		lidar.append(tmp)
-	return lidar
+  data = io.loadmat(file_name+".mat")
+  lidar = []
+  for m in data['lidar'][0]:
+    tmp = {}
+    tmp['t']= m[0][0][0]
+    nn = len(m[0][0])
+    if (nn != 5) and (nn != 6):     
+      raise ValueError("different length!")
+    tmp['pose'] = m[0][0][nn-4]
+    tmp['res'] = m[0][0][nn-3]
+    tmp['rpy'] = m[0][0][nn-2]
+    tmp['scan'] = m[0][0][nn-1]
+    
+    lidar.append(tmp)
+  return lidar
 
 def get_joint(file_name):
-	key_names_joint = ['acc', 'ts', 'rpy', 'gyro', 'pos', 'ft_l', 'ft_r', 'head_angles']
-	data = io.loadmat(file_name+".mat")
-	joint = {kn: data[kn] for kn in key_names_joint}
-	return joint
+  key_names_joint = ['acc', 'ts', 'rpy', 'gyro', 'pos', 'ft_l', 'ft_r', 'head_angles']
+  data = io.loadmat(file_name+".mat")
+  joint = {kn: data[kn] for kn in key_names_joint}
+  return joint
 
 
 def get_rgb(file_name):
-	key_names_rgb = ['t','width','imu_rpy','id','odom','head_angles','c','sz','vel','rsz','body_height','tr','bpp','name','height','image']
-	# image size: 1080x1920x3 uint8
-	data = io.loadmat(file_name+".mat")
-	data = data['RGB'][0]
-	rgb = []
-	for m in data:
-		tmp = {v:m[0][0][i] for (i,v) in enumerate(key_names_rgb)}
-		rgb.append(tmp)
-	return rgb
+  key_names_rgb = ['t','width','imu_rpy','id','odom','head_angles','c','sz','vel','rsz','body_height','tr','bpp','name','height','image']
+  # image size: 1080x1920x3 uint8
+  data = io.loadmat(file_name+".mat")
+  data = data['RGB'][0]
+  rgb = []
+  for m in data:
+    tmp = {v:m[0][0][i] for (i,v) in enumerate(key_names_rgb)}
+    rgb.append(tmp)
+  return rgb
 
 def get_depth(file_name):
-	key_names_depth = ['t','width','imu_rpy','id','odom','head_angles','c','sz','vel','rsz','body_height','tr','bpp','name','height','depth']
-	data = io.loadmat(file_name+".mat")
-	data = data['DEPTH'][0]
-	depth = []
-	for m in data:
-		tmp = {v:m[0][0][i] for (i,v) in enumerate(key_names_depth)}
-		depth.append(tmp)
-	return depth
-
-
-
-
-
+  key_names_depth = ['t','width','imu_rpy','id','odom','head_angles','c','sz','vel','rsz','body_height','tr','bpp','name','height','depth']
+  data = io.loadmat(file_name+".mat")
+  data = data['DEPTH'][0]
+  depth = []
+  for m in data:
+    tmp = {v:m[0][0][i] for (i,v) in enumerate(key_names_depth)}
+    depth.append(tmp)
+  return depth
